a5b3a4735115138a358086f41965b6a85f95e231
Aidan <amessmer@themessmers.net>
2026-02-09 10:10:28 -0800

    working on inventory part of the command

--- cmd/mysql.go
+++ cmd/mysql.go
@@ -1,38 +1,44 @@
 package cmd
 
 import (
-	"strconv"
 	"database/sql"
+	"errors"
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
+	"strconv"
+	"syscall"
 	"time"
-  "syscall"
-	"golang.org/x/term"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/spf13/cobra"
+	"golang.org/x/term"
 )
-var port int 
-var host string
-var username string
-var inventory bool
-var backup bool
-var restore bool
-var file string
-var dbName string
-var password string = ""
-var askedPass bool = false
+
+var (
+	port           int
+	host           string
+	username       string
+	inventory      bool
+	backup         bool
+	restore        bool
+	file           string
+	dbName         string = ""
+	cachedPassword string = ""
+	askedPass      bool   = false
+)
 
 var mysqlCmd = &cobra.Command{
-	Use: "mysql",
+	Use:   "mysql",
 	Short: "Module to Inventory Mysql.",
 	Long: `This command contains all functionality related to Mysql databases.
 
 This Module Contains the Following Functionality:
 - Backup a Database
 - Restore a Database
-- Inventory a Database`,	
-	RunE: runCmd,
+- Inventory a Database`,
+	RunE:         runCmd,
 	SilenceUsage: true,
 }
 
@@ -45,23 +51,22 @@ func init() {
 	mysqlCmd.Flags().BoolVarP(&backup, "backup", "b", false, "Should Backup")
 	mysqlCmd.Flags().BoolVarP(&restore, "restore", "r", false, "Should Restore")
 	mysqlCmd.Flags().StringVarP(&file, "file", "f", "", "File to Use for Backup/Restore")
-	mysqlCmd.Flags().StringVarP(&dbName, "dbName", "n", "", "Database name to Connect to")
+	// mysqlCmd.Flags().StringVarP(&dbName, "dbName", "n", "", "Database name to Connect to")
 
-	mysqlCmd.MarkFlagsMutuallyExclusive("backup", "restore")	
+	mysqlCmd.MarkFlagsMutuallyExclusive("backup", "restore")
 }
 
 func runCmd(cmd *cobra.Command, args []string) error {
- 
-	var didGetFlag = false
+	didGetFlag := false
 	if cmd.Flags().Changed("inventory") {
 		runInventory()
-		didGetFlag = true 
-	}	
+		didGetFlag = true
+	}
 
-	if  cmd.Flags().Changed("backup"){
+	if cmd.Flags().Changed("backup") {
 		runBackup()
 		didGetFlag = true
-	} else if  cmd.Flags().Changed("restore"){
+	} else if cmd.Flags().Changed("restore") {
 		runRestore()
 		didGetFlag = true
 	}
@@ -72,9 +77,80 @@ func runCmd(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func runInventory(){}
+func runInventory() {
+	password, err := getPassword()
+	if err != nil {
+		fmt.Println("failed to read password")
+		return
+	}
+	db, err := connectToDatabase("", "", host, port, dbName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
 
-func runBackup(){
+	printHeader("ANONYMOUS LOGIN TEST")
+	err = db.Ping()
+
+	if err != nil {
+		fmt.Printf("Server at %s allows ANONYMOUS login.\n", host)
+	} else {
+		fmt.Println("Anonymous login disabled")
+	}
+	db.Close()
+
+	db, err = connectToDatabase(username, password, host, port, dbName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer db.Close()
+
+	if db.Ping() != nil {
+		fmt.Printf("Error: SQL Authentication failed for %s@%s.\n", username, host)
+		return
+	}
+	userAccountsAndAuth(db)
+}
+
+func userAccountsAndAuth(db *sql.DB) {
+	printHeader("USER ACCOUNTS & AUTHENITCATION PLUGINS")
+	fmt.Printf("%-25s | %-15s | %-15s\n", "User@Host", "Plugin", "Password Set")
+	query := `
+		SELECT User, Host, plugin, 
+		IF(authentication_string='' OR Password='', 'NO', 'YES') 
+		FROM mysql.user;`
+
+	rows, err := db.Query(query)
+	if err != nil {
+		fmt.Println("Query Failed.")
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user, host, plugin, passSet string
+		if err := rows.Scan(&user, &host, &plugin, &passSet); err != nil {
+			fmt.Println("Error Reading Rows")
+			return
+		}
+
+		userHost := fmt.Sprintf("%s@%s", user, host)
+		fmt.Printf("%-25s | %-15s | %-15s\n", userHost, plugin, passSet)
+	}
+
+	if err = rows.Err(); err != nil {
+		fmt.Println("Error During Row Interation.")
+	}
+}
+
+func printHeader(header string) {
+	fmt.Println(header)
+	fmt.Println("-----------------------------------------------------")
+}
+
+func runBackup() {
 	if len(file) == 0 {
 		fmt.Println("This command requires -f to be specified")
 		return
@@ -85,7 +161,7 @@ func runBackup(){
 	password, err := getPassword()
 	if err != nil {
 		fmt.Errorf("failed to read password")
-		return 	
+		return
 	}
 
 	ofile, err := os.Create(file)
@@ -96,14 +172,14 @@ func runBackup(){
 	defer ofile.Close()
 
 	cmd := exec.Command("mysqldump",
-			"-u", username,
-			"-p"+password,
-			"-h", host,
-			"-P", strconv.Itoa(port),
-			"--all-databases",
-			"--events",
-			"--routines",
-			"--single-transaction",
+		"-u", username,
+		"-p"+password,
+		"-h", host,
+		"-P", strconv.Itoa(port),
+		"--all-databases",
+		"--events",
+		"--routines",
+		"--single-transaction",
 	)
 
 	cmd.Stdout = ofile
@@ -125,8 +201,8 @@ func CheckCliCmdExist(cmd string) bool {
 	return err == nil
 }
 
-func runRestore(){
-		if len(file) == 0 {
+func runRestore() {
+	if len(file) == 0 {
 		fmt.Println("This command requires -f to be specified")
 		return
 	} else if !CheckCliCmdExist("mysql") {
@@ -136,37 +212,35 @@ func runRestore(){
 	password, err := getPassword()
 	if err != nil {
 		fmt.Errorf("failed to read password")
-		return 	
-
-		ifile, err := os.Open(file)
-		if err != nil {
-			fmt.Println("Could not open specified file")
-			return
-		}
-		defer ifile.Close()
-
-		cmd := exec.command("mysql",
-				"-u", username,
-				"-p"+password,
-				"-h", host,
-				"-P", strconv.Itoa(port),
-		)
-		
-
-		cmd.Stdin = ifile
-		cmd.Stderr = os.Stderr
-
-		fmd.Printf("Restoring backup from %s...\n", file)
-		err = cmd.Run()
-		if err != nil {
-			fmt.Printf("Restore Failed: %w", err)
-			return
-		}
-
-		fmt.Println("Restoration completed successfully")
+		return
 	}
-}
+	ifile, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Could not open specified file")
+		return
+	}
+	defer ifile.Close()
 
+	cmd := exec.Command("mysql",
+		"-u", username,
+		"-p"+password,
+		"-h", host,
+		"-P", strconv.Itoa(port),
+	)
+
+	cmd.Stdin = ifile
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("Restoring backup from %s...\n", file)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Printf("Restore Failed: %w", err)
+		os.Remove(file)
+		return
+	}
+
+	fmt.Println("Restoration completed successfully")
+}
 
 func runDefault() error {
 	p, err := getPassword()
@@ -175,55 +249,58 @@ func runDefault() error {
 	}
 
 	db, err := connectToDatabase(username, p, host, port, dbName)
-
 	if err != nil {
 		return fmt.Errorf("")
 	}
-
-  err = db.Ping()	
-	if err != nil {
-		
-		return fmt.Errorf("MySQL connection failed: %v", err)
-  }
-
-  fmt.Println("MySQL connection successful!")
 	defer db.Close()
+	err = db.Ping()
+	if err != nil {
+		return fmt.Errorf("MySQL connection failed: %v", err)
+	}
+
+	fmt.Println("MySQL connection successful!")
+
 	return nil
 }
 
-func connectToDatabase(user string, password string, host string, port int, dbName string) (*sql.DB, error){
-	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true",username, password, host, port, dbName)
-	db, err := sql.Open("mysql", dns)	
-
+func connectToDatabase(user string, password string, host string, port int, dbName string) (*sql.DB, error) {
+	dns := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true", username, password, host, port, dbName)
+	db, err := sql.Open("mysql", dns)
 	if err != nil {
-		return  nil, fmt.Errorf("failed to connect to database")
+		return nil, fmt.Errorf("failed to connect to database")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		var netErr *net.OpError
+		if errors.As(err, &netErr) {
+			return nil, fmt.Errorf("DB not Listening")
+		}
 	}
 
 	db.SetConnMaxLifetime(time.Minute * 3)
 	db.SetMaxOpenConns(1)
 
 	fmt.Printf("Connecting to MySQL at %s:%d...\n", host, port)
-	return db, nil   
+	return db, nil
 }
 
-
 func getPassword() (string, error) {
- 		
-		if askedPass {
-			return password, nil
-		}
+	if askedPass {
+		return cachedPassword, nil
+	}
 
-		fmt.Print("Enter Database Password: ")
-    
-    // syscall.Stdin is the file descriptor for standard input
-    // ReadPassword disables terminal echo automatically
-    bytePassword, err := term.ReadPassword(int(syscall.Stdin))
-    if err != nil {
-        return "", err
-    }
-    
-    fmt.Println() // Print a newline because ReadPassword doesn't
-    password = string(bytePassword)
-		askedPass = true
-		return string(bytePassword), nil
+	fmt.Print("Enter Database Password: ")
+
+	// syscall.Stdin is the file descriptor for standard input
+	// ReadPassword disables terminal echo automatically
+	bytePassword, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return "", err
+	}
+
+	fmt.Println() // Print a newline because ReadPassword doesn't
+	cachedPassword = string(bytePassword)
+	askedPass = true
+	return string(bytePassword), nil
 }
